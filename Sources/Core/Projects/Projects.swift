4f587144bf6e83c804ae8c57dcaeef7bd46feb96
Sp3cker <speker97@protonmail.com>
2025-09-27 23:12:11 -0500

    save load projects

---
+++ Sources/Core/Projects/Projects.swift
@@ -0,0 +1,7 @@
+//
+//  Project.swift
+//  VoicegroupParser
+//
+//  Created by Spencer on 9/27/25.
+//
+
---
+++ Sources/Projects/Projects.swift
@@ -0,0 +1,10 @@
+//
+//  Project.swift
+//  VoicegroupParser
+//
+//  Created by Spencer on 9/27/25.
+//
+
+class Projects {
+  
+}
